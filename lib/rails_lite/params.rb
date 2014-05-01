cba4679abcf25ca23cdb2178f28699194c9a49f7
Bryan Phillips <bdphilly@gmail.com>
2014-04-30 18:00:50 -0700

    Parsing & Router

--- lib/rails_lite/params.rb
+++ lib/rails_lite/params.rb
@@ -6,11 +6,28 @@ class Params
   # 2. post body
   # 3. route params
   def initialize(req, route_params = {})
-    @params = req.query_string
+    @params = route_params
+    
+    if req.query_string
+      nested_hash = parse_www_encoded_form(req.query_string)
+      @params.merge!(nested_hash)
+    end
+
+    if req.body
+      post_body = parse_www_encoded_form(req.body)
+      @params.merge!(post_body)
+    end
+
+      # @params.merge!(parse_www_encoded_form(req.body))
+    # req.query_string.merge(req.body).merge(route_params)
+    
+
+      # @params.merge!(parse_www_encoded_form(req.body))
 
   end
 
   def [](key)
+    @params[key]
   end
 
   def permit(*keys)
@@ -30,15 +47,37 @@ class Params
   private
   # this should return deeply nested hash
   # argument format
-  # user[address][street]=main&user[address][zip]=89436
+  # "user[address][street]=main&user[address][zip]=89436"
   # should return
   # { "user" => { "address" => { "street" => "main", "zip" => "89436" } } }
   def parse_www_encoded_form(www_encoded_form)
-    decode_www_form(www_encoded_form)
+    # decoded = URI::decode_www_form(www_encoded_form)
+    # p www
+    pair_strings = www_encoded_form.split('&')
+
+    out = {}
+
+    pair_strings.each do |pair_string|
+      key, value = pair_string.split('=')
+      key_array = parse_key(key)
+
+      hash_we_are_at = out
+      key_array[0...-1].each do |key_thing|
+        unless hash_we_are_at.include? key_thing
+          hash_we_are_at[key_thing] = {}
+        end
+        hash_we_are_at = hash_we_are_at[key_thing]
+      end
+      hash_we_are_at[key_array.last] = value
+    end
+
+    out
+
   end
 
   # this should return an array
   # user[address][street] should return ['user', 'address', 'street']
   def parse_key(key)
+    key.to_s.split("[").map {|component|component.gsub(/]/,"")}
   end
 end
--- lib/rails_lite/router.rb
+++ lib/rails_lite/router.rb
@@ -1,11 +1,20 @@
+require 'uri'
+require 'webrick'
+
 class Route
   attr_reader :pattern, :http_method, :controller_class, :action_name
 
   def initialize(pattern, http_method, controller_class, action_name)
+    @pattern = pattern
+    @http_method = http_method
+    @controller_class = controller_class
+    @action_name = action_name
   end
 
   # checks if pattern matches path and method matches request method
   def matches?(req)
+    @http_method == req.request_method.downcase.to_sym
+    @pattern.match(req.path)
   end
 
   # use pattern to pull out route params (save for later?)
@@ -18,10 +27,12 @@ class Router
   attr_reader :routes
 
   def initialize
+    @routes = []
   end
 
   # simply adds a new route to the list of routes
   def add_route(pattern, method, controller_class, action_name)
+    @routes << Route.new(pattern, method, controller_class, action_name)
   end
 
   # evaluate the proc in the context of the instance
@@ -32,13 +43,16 @@ class Router
   # make each of these methods that
   # when called add route
   [:get, :post, :put, :delete].each do |http_method|
+    define_method(http_method) { add_route }
   end
 
   # should return the route that matches this request
   def match(req)
+    @routes.find { |route| route.matches?(req) }
   end
 
   # either throw 404 or call run on a matched route
   def run(req, res)
+    res.status = 404 unless match(req)
   end
 end
